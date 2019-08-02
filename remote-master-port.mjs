340563c8d6b6b80ed1991a17953dc0b8caccfce7
Zoran Obradović <zockyo@gmail.com>
2019-08-02 19:10:53 +0200

    prepublish

--- remote-master-port.mjs
+++ remote-master-port.mjs
@@ -4,10 +4,11 @@ export class RemoteMasterPort {
     await it.connect();
     return it;
   }
-  constructor(id, iframe, origin="*") {
+  constructor(id, iframe, {origin="*",debug=false}) {
     this.id = id;
     this.iframe = iframe;
     this.origin = origin;
+    this.debug = debug;
     this.connected = false;
     this.active = true;
     this._handlers = {};
@@ -20,44 +21,49 @@ export class RemoteMasterPort {
     return new Promise((resolve, reject) => {
       
       const timeout = setTimeout(
-        ()=>reject(new Error("connect-timeout"))
+        ()=>reject(new Error("port-connect-timeout"))
         ,5000
       );
-
+      
       this.iframe.contentWindow.postMessage({ [this.id]: "connect", "version": "1.0.0", port: this.channel.port2 }, this.origin, [this.channel.port2]);
       this.port.onmessage = ev => {
         try {
           if (!ev.data || ev.data[this.id] !== "connected") return this._deactivate();
-          this.manifest = ev.data.manifest || {}
+          clearTimeout(timeout);
+          const manifest = ev.data.manifest || {}
           this.connected = true;
           this.port.onmessage = ev => {
             this._receive(ev.data || {});
           };
-          resolve();
+          resolve(manifest);
         } catch (err) {
           reject(err);
         }
       };
     });
   }
+  disconnect() {
+    this.send('port-disconnect');
+    this._deactivate();
+  }
   on(event,handler) {
     this._handlers[event]=handler;
   }
-  request(api, cmd, args = {}, transfer = []) {
-    if (!this.connected) return;
+  request(cmd, args = {}, transfer = []) {
+    if (!this.connected) throw new Error("port-not-connected");
     const rsvp = Math.random().toString(36).substr(2);
     const promise = new Promise((resolve, reject) => {
       this._pending[rsvp] = { resolve, reject };
     });
-    this._send({ api, cmd, args, rsvp }, transfer);
+    this._send({ cmd, args, rsvp }, transfer);
     return promise;
   }
-  send(api, cmd, args = [], transfer = []) {
-    if (!this.connected) return;
-    this._send({ api, cmd, args }, transfer);
+  send(cmd, args = [], transfer = []) {
+    if (!this.connected) throw new Error("port-not-connected");
+    this._send({ cmd, args }, transfer);
   }
   _send(msg, transfer = []) {
-    if (!this.connected) return;
+    if (!this.connected) throw new Error("port-not-connected");
     this.port.postMessage(msg, transfer);
   }
   _receive({ event, data, re, result, error }) {
